1c00120bd4eed541bf69a7833c10fd379eaf1af7
jbhunt <hunt.brian.joshua@gmail.com>
2025-02-11 14:31:38 -0700

    Finished working on the cli

--- saccade_extraction/cli.py
+++ saccade_extraction/cli.py
@@ -1,15 +1,39 @@
 import click
+from saccade_extraction.project import extractRealSaccades
 
 @click.group()
 def cli():
     pass
 
 @cli.command()
-@click.argument('folder', type=str)
-def extract(folder):
+@click.argument('config', type=str, required=True)
+@click.argument('dlc', type=str, required=True)
+@click.argument('ifi', type=str, required=True)
+@click.option('--model', type=int, default=-1, help='Model index (default is -1)')
+def extract(
+    config,
+    dlc,
+    ifi,
+    model
+    ):
     """
-    Extract saccades
+    Extract real saccades
+
+    \b
+    CONFIG: Path to the config file
+    DLC: Path to the DeepLabCut pose estimates
+    IFI: Path to the file that stores inter-frame intervals
     """
+
+    fileSets = [(
+        dlc,
+        ifi,
+    ),]
+    extractRealSaccades(
+        config,
+        fileSets,
+        model
+    )
 
     return
 
