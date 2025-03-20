f17bb984c27a43818f6189b1a9f4460de92368eb
jbhunt <hunt.brian.joshua@gmail.com>
2025-03-20 17:32:50 -0600

    Got rid of broken import

--- saccade_extraction/__init__.py
+++ saccade_extraction/__init__.py
@@ -1,4 +1,4 @@
 from saccade_extraction.models import createTrainingDataset, trainModels
 from saccade_extraction.project import initializeProject, collectFileSets, addNewSessions, labelPutativeSaccades, extractRealSaccades
 from saccade_extraction.validate import visualizePredictions, quantifyPerformance
-from saccade_extraction.pose import loadPoseEstimates, extractPutativeSaccades
+from saccade_extraction.pose import extractPutativeSaccades
